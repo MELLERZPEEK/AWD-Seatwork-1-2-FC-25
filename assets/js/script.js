48c90421622dc0873ff2cb4fc48af57a6dbfa7aa
MELLERZPEEK <167728686+MELLERZPEEK@users.noreply.github.com>
2025-02-03 23:03:22 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // Car data with image URLs and rental prices
 const cars = [
-    { id: 1, name: "Toyota Corolla", image: "images/toyota.jpg", price: "₱3,000/day", status: "Available" },
-    { id: 2, name: "Honda Civic", image: "images/honda.jpg", price: "₱3,500/day", status: "Available" },
-    { id: 3, name: "Ford Focus", image: "images/ford.jpg", price: "₱2,800/day", status: "Available" },
-    { id: 4, name: "Tesla Model 3", image: "images/tesla.jpg", price: "₱6,000/day", status: "Available" },
-    { id: 5, name: "Nissan Altima", image: "images/nissan.jpg", price: "₱3,200/day", status: "Available" },
-    { id: 6, name: "BMW 3 Series", image: "images/bmw.jpg", price: "₱5,500/day", status: "Available" },
-    { id: 7, name: "Mercedes-Benz C-Class", image: "images/mercedes.jpg", price: "₱6,500/day", status: "Available" },
-    { id: 8, name: "Hyundai Elantra", image: "images/hundai.jpg", price: "₱2,900/day", status: "Available" },
-    { id: 9, name: "Chevrolet Malibu", image: "images/chevrolet.jpg", price: "₱3,300/day", status: "Available" },
-    { id: 10, name: "Audi A4", image: "images/audi.jpg", price: "₱7,000/day", status: "Available" }
+    { id: 1, name: "Toyota Corolla", image: "img/toyota.jpg", price: "₱3,000/day", status: "Available" },
+    { id: 2, name: "Honda Civic", image: "img/honda.jpg", price: "₱3,500/day", status: "Available" },
+    { id: 3, name: "Ford Focus", image: "img/ford.jpg", price: "₱2,800/day", status: "Available" },
+    { id: 4, name: "Tesla Model 3", image: "img/tesla.jpg", price: "₱6,000/day", status: "Available" },
+    { id: 5, name: "Nissan Altima", image: "img/nissan.jpg", price: "₱3,200/day", status: "Available" },
+    { id: 6, name: "BMW 3 Series", image: "img/bmw.jpg", price: "₱5,500/day", status: "Available" },
+    { id: 7, name: "Mercedes-Benz C-Class", image: "img/mercedes.jpg", price: "₱6,500/day", status: "Available" },
+    { id: 8, name: "Hyundai Elantra", image: "img/hundai.jpg", price: "₱2,900/day", status: "Available" },
+    { id: 9, name: "Chevrolet Malibu", image: "img/chevrolet.jpg", price: "₱3,300/day", status: "Available" },
+    { id: 10, name: "Audi A4", image: "img/audi.jpg", price: "₱7,000/day", status: "Available" }
 ];
 
 // Display Cars
