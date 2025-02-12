3ee90e2c10498b038d07cbb57800680e26b0bbf3
MELLERZPEEK <167728686+MELLERZPEEK@users.noreply.github.com>
2025-02-12 23:17:06 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 let cars = JSON.parse(localStorage.getItem("cars")) || [
-    { id: 1, name: "Toyota Corolla", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/toyota.jpg", price: "₱3,000/day", status: "Available" },
-    { id: 2, name: "Honda Civic", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/honda.jpg", price: "₱3,500/day", status: "Available" },
-    { id: 3, name: "Ford Focus", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/ford.jpg", price: "₱2,800/day", status: "Available" },
-    { id: 4, name: "Tesla Model 3", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/tesla.jpg", price: "₱6,000/day", status: "Available" },
-    { id: 5, name: "Nissan Altima", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/nissan.jpg", price: "₱3,200/day", status: "Available" },
-    { id: 6, name: "BMW 3 Series", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/bmw.jpg", price: "₱5,500/day", status: "Available" },
-    { id: 7, name: "Mercedes-Benz C-Class", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/mercedes.jpg", price: "₱6,500/day", status: "Available" },
-    { id: 8, name: "Hyundai Elantra", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/hyundai.jpg", price: "₱2,900/day", status: "Available" },
-    { id: 9, name: "Chevrolet Malibu", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/chevrolet.jpg", price: "₱3,300/day", status: "Available" },
-    { id: 10, name: "Audi A4", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/images/audi.jpg", price: "₱7,000/day", status: "Available" }
+    { id: 1, name: "Toyota Corolla", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/toyota.jpg", price: "₱3,000/day", status: "Available" },
+    { id: 2, name: "Honda Civic", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/honda.jpg", price: "₱3,500/day", status: "Available" },
+    { id: 3, name: "Ford Focus", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/ford.jpg", price: "₱2,800/day", status: "Available" },
+    { id: 4, name: "Tesla Model 3", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/tesla.jpg", price: "₱6,000/day", status: "Available" },
+    { id: 5, name: "Nissan Altima", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/nissan.jpg", price: "₱3,200/day", status: "Available" },
+    { id: 6, name: "BMW 3 Series", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/bmw.jpg", price: "₱5,500/day", status: "Available" },
+    { id: 7, name: "Mercedes-Benz C-Class", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/mercedes.jpg", price: "₱6,500/day", status: "Available" },
+    { id: 8, name: "Hyundai Elantra", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/hyundai.jpg", price: "₱2,900/day", status: "Available" },
+    { id: 9, name: "Chevrolet Malibu", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/chevrolet.jpg", price: "₱3,300/day", status: "Available" },
+    { id: 10, name: "Audi A4", image: "https://mellerzpeek.github.io/AWD-Seatwork-1-2-FC-25/assets/img/audi.jpg", price: "₱7,000/day", status: "Available" }
 ];
 
 
